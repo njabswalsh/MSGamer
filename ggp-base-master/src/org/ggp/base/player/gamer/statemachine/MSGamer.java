f92f8edeb3eecae6246686e899cd9e381f019c40
njabswalsh <njwalsh@stanford.edu>
2014-04-16 16:58:05 -0700

    First Commit

---
+++ ggp-base-master/src/org/ggp/base/player/gamer/statemachine/MSGamer.java
@@ -0,0 +1,64 @@
+package org.ggp.base.player.gamer.statemachine;
+
+import java.util.List;
+import java.util.Random;
+
+import org.ggp.base.player.gamer.exception.GamePreviewException;
+import org.ggp.base.util.game.Game;
+import org.ggp.base.util.statemachine.MachineState;
+import org.ggp.base.util.statemachine.Move;
+import org.ggp.base.util.statemachine.Role;
+import org.ggp.base.util.statemachine.StateMachine;
+import org.ggp.base.util.statemachine.exceptions.GoalDefinitionException;
+import org.ggp.base.util.statemachine.exceptions.MoveDefinitionException;
+import org.ggp.base.util.statemachine.exceptions.TransitionDefinitionException;
+import org.ggp.base.util.statemachine.implementation.prover.ProverStateMachine;
+
+public class MSGamer extends StateMachineGamer {
+
+	@Override
+	public StateMachine getInitialStateMachine() {
+		return new ProverStateMachine();
+	}
+
+	@Override
+	public void stateMachineMetaGame(long timeout)
+			throws TransitionDefinitionException, MoveDefinitionException,
+			GoalDefinitionException {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public Move stateMachineSelectMove(long timeout)
+			throws TransitionDefinitionException, MoveDefinitionException,
+			GoalDefinitionException {
+		StateMachine stateMachine = getStateMachine();
+		MachineState currentState = getCurrentState();
+		Role role = getRole();
+		List<Move> legalMoves = stateMachine.getLegalMoves(currentState, role);
+		Random r = new Random();
+		return legalMoves.get(r.nextInt((legalMoves.size())));
+	}
+
+	@Override
+	public void stateMachineStop() {
+
+	}
+
+	@Override
+	public void stateMachineAbort() {
+
+	}
+
+	@Override
+	public void preview(Game g, long timeout) throws GamePreviewException {
+
+	}
+
+	@Override
+	public String getName() {
+		return "MSGamer";
+	}
+
+}
